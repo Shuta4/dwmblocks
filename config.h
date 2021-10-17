bc3120e78772d3015abec68fea67ef401ab7478a
Shuta4 <skirdin.nik@gmail.com>
2021-10-17 14:47:01 +0300

    Updated config

--- config.h
+++ config.h
@@ -1,16 +1,9 @@
 #define CMDLENGTH 50
-#define DELIMITER "<"
+#define DELIMITER " | "
 #define TRAILING_DELIMITER
-#define CLICKABLE_BLOCKS
+/* #define CLICKABLE_BLOCKS */
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17)
-	BLOCK("sb-music",   0,    18)
-	BLOCK("sb-disk",    1800, 19)
-	BLOCK("sb-memory",  1800, 20)
-	BLOCK("sb-loadavg", 1800, 21)
-	BLOCK("sb-volume",  0,    22)
-	BLOCK("sb-battery", 5,    23)
-	BLOCK("sb-date",    1,    24)
-	BLOCK("sb-network", 5,    25)
+	BLOCK("echo \"BAT:$(cat /sys/class/power_supply/BAT1/capacity)\"%", 5,    23)
+	BLOCK("date '+%Y-%m-%d %H:%M:%S'",                                  1,    24)
 };
