163b89944aa37a642f9d04809a55d257164f811b
Shuta4 <skirdin.nik@gmail.com>
2021-10-23 23:26:23 +0300

    Added more blocks

--- config.h
+++ config.h
@@ -4,7 +4,10 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
+	BLOCK("dwmblocks-disk",    60,   5)
+	BLOCK("dwmblocks-network", 10,   4)
 	BLOCK("dwmblocks-battery", 5,    3)
 	BLOCK("dwmblocks-volume",  30,   2)
+	BLOCK("dwmblocks-kb",      60,   6)
 	BLOCK("dwmblocks-time",    1,    1)
 };
