1c48443c23de446ade765d88209adffafa7fdada
Shuta4 <skirdin.nik@gmail.com>
2021-11-13 00:57:16 +0300

    Changed update timeout for network block

--- config.h
+++ config.h
@@ -5,9 +5,10 @@
 
 const Block blocks[] = {
 	BLOCK("dwmblocks-disk",    60,   5)
-	BLOCK("dwmblocks-network", 10,   4)
+	BLOCK("dwmblocks-network", 3,    4)
 	BLOCK("dwmblocks-battery", 5,    3)
 	BLOCK("dwmblocks-volume",  30,   2)
 	BLOCK("dwmblocks-kb",      60,   6)
 	BLOCK("dwmblocks-time",    1,    1)
 };
+
